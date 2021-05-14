3fa62707644f0cd223efdd4905f16964d8823dfc
nivek29 <kevinmezamc@gmail.com>
2021-05-14 13:25:48 -0600

    0.0.5

--- src/components/baseTest.js
+++ src/components/baseTest.js
@@ -3,7 +3,7 @@ const nombre   = 'Fernando';
 const apellido = 'Herrera';
 
 
-// const nombreCompleto = nombre + ' ' + apellido
+// const nombreCompleto = nombre + ' ' + apellidos
 const nombreCompleto = `${ nombre } ${ apellido }`;
 
 // console.log( nombreCompleto );
