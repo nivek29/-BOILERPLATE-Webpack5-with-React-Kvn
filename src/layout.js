9261d5440934ab486b4612488dec9f541ee68778
nivek29 <kevinmezamc@gmail.com>
2021-05-27 17:03:26 -0600

    da<2

--- src/layout.js
+++ src/layout.js
@@ -10,11 +10,10 @@ export const Layout = () => {
 			<Router>
 				<Switch>
 					<Route exact path="/">
-						<Dependencies />
 						<Home></Home>
 					</Route>
 					<Route exact path="/dependencies">
-						
+						<Dependencies />
 					</Route>
 				</Switch>
 			</Router>
